f12ded4c3df934818b1c66fadcd81b001eed82e2
jakobrs <jakobrs100@gmail.com>
2021-08-28 19:35:08 +0100

    Allow using custom colour with fill.rs

--- examples/fill.rs
+++ examples/fill.rs
@@ -1,12 +1,27 @@
 use tasbot_display::{Display, RgbColor};
 use tasbot_display::tasbot::NUM_PIXELS;
 
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(setting = structopt::clap::AppSettings::DeriveDisplayOrder)]
+struct Opts {
+    #[structopt(short, long, default_value = "0")]
+    red: u8,
+    #[structopt(short, long, default_value = "255")]
+    green: u8,
+    #[structopt(short, long, default_value = "0")]
+    blue: u8,
+}
+
 fn main() {
+    let opts = Opts::from_args();
+    let color = RgbColor::from([opts.red, opts.green, opts.blue]);
+
     let mut display = Display::new(NUM_PIXELS);
 
-    let red = RgbColor::from([0, 255, 0]);
     for i in 0..NUM_PIXELS {
-        display[i as usize] = red;
+        display[i as usize] = color;
     }
 
     display.draw();
