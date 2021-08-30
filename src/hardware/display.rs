ca431d26cc4337952eb7164db4e1eda711b119cf
jakobrs <jakobrs100@gmail.com>
2021-08-30 19:13:55 +0200

    Fix dont-cap-brightness feature

--- src/hardware/display.rs
+++ src/hardware/display.rs
@@ -3,7 +3,7 @@ use std::ops::{Index, IndexMut};
 use crate::hardware::neopixel_device::NeoPixelDevice;
 use crate::RgbColor;
 
-const MAX_BRIGHTNESS: f32 = if cfg!(dont_cap_brightness) { 1. } else { 0.1 };
+const MAX_BRIGHTNESS: f32 = if cfg!(feature = "dont-cap-brightness") { 1. } else { 0.1 };
 const DEFAULT_BRIGHTNESS: f32 = 0.1;
 
 pub struct Display {
