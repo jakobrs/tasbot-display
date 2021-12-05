4a1b538b422f85fc0ff1638a21241910e3518723
jakobrs <jakobrs100@gmail.com>
2021-12-05 22:14:56 +0100

    image binary has been changed from bins -> examples, fix package.sh

--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@
 mkdir -p out
 rm out/*
 
-cp -p target/arm-unknown-linux-musleabihf/$1/{image,examples/{fill,clear,flashes,dot}} out/
+cp -p target/arm-unknown-linux-musleabihf/$1/examples/{image,fill,clear,flashes,dot} out/
 
 armv7l-unknown-linux-gnueabihf-strip out/*
 
